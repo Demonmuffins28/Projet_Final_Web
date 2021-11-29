0c1c89b8cb61122cc9c9c1e16437d44b9e2035cf
AmineBenSlimanee <58090140+AmineBenSlimanee@users.noreply.github.com>
2021-11-29 16:32:35 -0500

    Revert "Merge pull request #3 from Demonmuffins28/revert-2-bare-de-recherche-et-filtre"
    This reverts commit 3e758e4e03db325450fe653900fc71ca3bc80561, reversing
    changes made to 7b1b037db15f87647c3795688a79e149b077d884.

--- Projet_Final_Web/Controllers/DVDController.cs
+++ Projet_Final_Web/Controllers/DVDController.cs
@@ -15,8 +15,7 @@ namespace Projet_Final_Web.Controllers
         private readonly DbContextProjetFinal _context;
         private readonly SignInManager<Utilisateurs> _signInManager;
         private readonly UserManager<Utilisateurs> _userManager;
-        private Utilisateurs UtilisateurActuel;
-        private DVDViewModel model;
+        private static DVDViewModel model;
 
         public DVDController(DbContextProjetFinal context, SignInManager<Utilisateurs> signInManager, UserManager<Utilisateurs> userManager)
         {
@@ -31,15 +30,15 @@ namespace Projet_Final_Web.Controllers
             if (!_signInManager.IsSignedIn(User))
                 return RedirectToAction("Login", "Account");
 
-            if (UtilisateurActuel == null)
-                UtilisateurActuel = await _userManager.GetUserAsync(HttpContext.User);
-
             if (model == null)
             {
                 await initialiserModel(page);
             }
             else
             {
+                model.utilisateursActuel = await _userManager.GetUserAsync(HttpContext.User);
+                model.TitreRechercher = "";
+                await compterNombrePage();
                 await changerPage(page);
             }
 
@@ -47,64 +46,100 @@ namespace Projet_Final_Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(string DVDFiltreSelectionner)
+        public async Task<IActionResult> Index(DVDViewModel DVDModel)
         {
-            return View();
+            model.TitreRechercher = DVDModel.TitreRechercher;
+            model.TrierPar = DVDModel.TrierPar;
+            await compterNombrePage();
+            await changerPage(1);
+            model.utilisateursActuel = await _userManager.GetUserAsync(HttpContext.User);
+            return View(model);
+        }
+
+
+
+        [NonAction]
+        private async Task<int> getNbDVDParPage()
+        {
+            bool aPreferenceDVDParPage = await _context.ValeursPreferences.Where(v => v.NoUtilisateur == model.utilisateursActuel.Id && v.NoPreference == 7).FirstOrDefaultAsync() != null;
+            return aPreferenceDVDParPage ? Convert.ToInt32((await _context.ValeursPreferences.Where(v => v.NoUtilisateur == model.utilisateursActuel.Id && v.NoPreference == 7).FirstOrDefaultAsync()).Valeur) : 12;
         }
 
         [NonAction]
-        private async Task<int> getDVDParPage()
+        private async Task<List<Films>> getDVDParPage(int page)
         {
-            bool aPreferenceDVDParPage = await _context.ValeursPreferences.Where(v => v.NoUtilisateur == UtilisateurActuel.Id && v.NoPreference == 7).FirstOrDefaultAsync() != null;
-            return aPreferenceDVDParPage ? Convert.ToInt32((await _context.ValeursPreferences.Where(v => v.NoUtilisateur == UtilisateurActuel.Id && v.NoPreference == 7).FirstOrDefaultAsync()).Valeur) : 12;
+            int nbDVDParPage = await getNbDVDParPage();
+            return (await getDVD()).Skip(nbDVDParPage * (page - 1)).Take(nbDVDParPage).ToList();
         }
 
         [NonAction]
-        private async Task<IEnumerable<Films>> getDVD(int page)
+        private async Task<List<Films>> getDVD()
         {
-            int DVDParPage = await getDVDParPage();
-            return await _context.Films.Skip(DVDParPage * (page-1)).Take(DVDParPage).ToListAsync();
+            List<Films> listDVD;
+            if (model.TitreRechercher == null)
+            {
+                listDVD = await _context.Films.ToListAsync();
+            }
+            else
+            {
+                listDVD =  await _context.Films.Where(a => a.TitreFrancais.Contains(model.TitreRechercher) || a.TitreOriginal.Contains(model.TitreRechercher)).ToListAsync();
+            }
+
+            switch (model.TrierPar)
+            {
+                case 1:
+                    listDVD = (from unFilm in listDVD
+                              join user in await _userManager.Users.ToListAsync()
+                              on unFilm.NoUtilisateurMAJ equals user.Id
+                              orderby user.UserName
+                              select unFilm).ToList();
+                    break;
+                case 2:
+                    listDVD = listDVD.OrderBy(f => f.TitreFrancais).ToList();
+                    break;
+                case 3:
+                    listDVD = (from unFilm in listDVD
+                               join user in await _userManager.Users.ToListAsync()
+                               on unFilm.NoUtilisateurMAJ equals user.Id
+                               orderby user.UserName, unFilm.TitreFrancais
+                               select unFilm).ToList();
+                    break;
+            }
+
+            return listDVD;
         }
 
         [NonAction]
         private async Task initialiserModel(int page)
         {
             int nbDVDTotal = (await _context.Films.ToListAsync()).Count;
-            int DVDParPage = await getDVDParPage();
 
-            model = new DVDViewModel
-            {
-                nbPage = (nbDVDTotal + DVDParPage - 1) / DVDParPage,
-                utilisateursActuel = UtilisateurActuel
-            };
+            model = new DVDViewModel();
+            model.utilisateursActuel = await _userManager.GetUserAsync(HttpContext.User);
+            await compterNombrePage();
+            model.TitreRechercher = "";
+            
             await changerPage(page);
+        }
+
+        [NonAction]
+        private async Task compterNombrePage()
+        {
+            int DVDParPage = await getNbDVDParPage();
+            model.nbPage = ((await getDVD()).Count() + DVDParPage - 1) / DVDParPage;
         }
 
         [NonAction]
         private async Task changerPage(int page)
         {
-            model.listDVD = new List<Tuple<Films, int>>();
-
-            foreach (Films DVD in await getDVD(page))
-            {
-                model.listDVD.Add(new Tuple<Films, int> (DVD, await getIdUtilisateurDVDEnMain(DVD)));
-            }
-            
+            model.listDVD = await getDVDParPage(page);
             model.page = page;
         }
 
         [NonAction]
-        private async Task<int> getIdUtilisateurDVDEnMain(Films DVD)
+        private async Task<Utilisateurs> GetUtilisateursByID(string Id)
         {
-            return Convert.ToInt32(
-                (await 
-                _context.EmpruntsFilms
-                .Where(v => v.NoExemplaire.ToString().Substring(0, v.NoExemplaire.ToString().Length - 2) == DVD.NoFilm.ToString())
-                .OrderByDescending(a => a.DateEmprunt)
-                .FirstOrDefaultAsync())
-                .NoUtilisateur
-            );
+            return await _userManager.FindByIdAsync(Id);
         }
-
     }
 }
--- Projet_Final_Web/ViewModel/DVDViewModel.cs
+++ Projet_Final_Web/ViewModel/DVDViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Projet_Final_Web.Models;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Projet_Final_Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +9,24 @@ namespace Projet_Final_Web.ViewModel
 {
     public class DVDViewModel
     {
-        public List<Tuple<Films, int>> listDVD { get; set; }
+
+        public List<Films> listDVD { get; set; }
 
         public int page { get; set; }
 
         public int nbPage { get; set; }
 
-        public string DVDFiltreSelectionner { get; set; }
+        public string TitreRechercher { get; set; }
 
         public Utilisateurs utilisateursActuel { get; set; }
+
+        public int TrierPar { get; set; } = 1;
+
+        public List<SelectListItem> ListTrie { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "1", Text = "Noms d’utilisateurs"},
+            new SelectListItem { Value = "2", Text = "Titres français de DVD"},
+            new SelectListItem { Value = "3", Text = "Noms d’utilisateurs et Titres français de DVD"},
+        };
     }
 }
